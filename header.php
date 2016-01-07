9badf31ae38794b30cd1e5f7bcc6c6557fc659ae
sezj <sarahzachrich@gmail.com>
2016-01-07 13:32:55 -0500

    copy changes from HWCOE starter theme

--- header.php
+++ header.php
@@ -47,9 +47,8 @@
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
 				<div id="inner-header" class="wrap cf">
-
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<a href="http://ufl.edu" id="uf-logo"><img src="<?php echo get_template_directory_uri(); ?>/library/images/uf-signature.png" alt="University of Florida" /></a>
+					<h1 id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
